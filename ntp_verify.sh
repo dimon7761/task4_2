72564c20853114b1d2032451cbb4121613f4a251
root <root@dell.dlnet.kharkov.com>
2018-03-31 22:04:47 +0300

    Update

--- ntp_verify.sh
+++ ntp_verify.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #Dmitriy Litvin 2018
-#set -x
 
 #CHECK PROC
 if [ ! -n  "$(service ntp status | grep -w active)" ]; then `service ntp restart`; echo 'NOTICE: ntp is not running'; fi
 
+
 #CHECK FILE
-if [ ! -f  "/etc/ntp.conf.bak" ]; then cp /etc/ntp.conf /etc/ntp.conf.bak; echo 'NOTICE: bak file corrupt'; fi
+if [ ! -f  "/etc/ntp.conf" -a ! -f  "/etc/ntp.conf.bak" ]; then echo 'NOTICE: ups...'; fi
+if [ ! -f  "/etc/ntp.conf.bak" ]; then cp /etc/ntp.conf /etc/ntp.conf.bak; echo 'NOTICE: bak file restore'; fi
+if [ ! -f  "/etc/ntp.conf" ]; then cp /etc/ntp.conf.bak /etc/ntp.conf; echo 'NOTICE: conf file restore'; fi
 
 #CHEK CHANGE
 if [ -n "$(diff /etc/ntp.conf.bak /etc/ntp.conf)" ]; then 
